b833977c179f1b391c968229990588d26be9fd3b
Paritosh Pundir <0p0a0r0i@gmail.com>
2018-12-14 00:21:03 +0530

    Extension execution permission

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {});
+chrome.runtime.onInstalled.addListener(function() {
+  // take tab permission
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {
+          schemes: ['https', 'http']
+        },
+      })
+      ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
