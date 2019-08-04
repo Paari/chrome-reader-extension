e2fbae0df0180967ae38e3e0b543a3fb8d60afce
Paari <0p0a0r0i@gmail.com>
2019-08-04 22:42:34 +0530

    🐞BUG: Issue #2 - Fixed the main content of the page scrolling in the background

--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,15 @@
 // check if dom already contains root
 if (document.getElementById('rr-root') === null) {
+  // make a reference to the body
+  // the page will not refresh, so make reference on first load
+  var body = document.getElementsByTagName('body');
+
   // add the node to the DOM
   let newNode = document.createElement('div');
   newNode.setAttribute("id", "rr-root");
   document.body.append(newNode);
 }
+
+// Make the page not scroll when the extension is active
+body[0].style.height = '100vh';
+body[0].style.overflow = 'hidden';
